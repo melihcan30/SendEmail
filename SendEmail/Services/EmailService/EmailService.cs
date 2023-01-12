a0491eccb84c0d2db05375e286305aba2ff711ae
Melihcan Serbest <canmelih.free3@gmail.com>
2023-01-12 18:32:21 +0300

    Use the AppSettings for Host

--- SendEmail/Services/EmailService/EmailService.cs
+++ SendEmail/Services/EmailService/EmailService.cs
@@ -7,17 +7,24 @@ namespace SendEmail.Services.EmailService
 {
     public class EmailService : IEmailService
     {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void SendEmail(EmailDto request)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("jack.wintheiser43@ethereal.email"));
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
             email.To.Add(MailboxAddress.Parse(request.To));
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
 
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("jack.wintheiser43@ethereal.email", "DsE1FvDUW89cGKFZZR");
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
             smtp.Send(email);
             smtp.Disconnect(true);
         }
